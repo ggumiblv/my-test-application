6c4e912fa55ad98c26e9073f3f703cc6275a7c5c
ggumiblv <gulnazhakimova70@gmail.com>
2024-09-24 19:03:22 +0200

    clean smth

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import ReactDOM from 'react-dom/client'; // Изменено
+import ReactDOM from 'react-dom/client'; 
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
 
-// Найти корневой элемент
 const rootElement = document.getElementById('root') as HTMLElement;
 
-// Создать корневой рендер
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <Provider store={store}>
